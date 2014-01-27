0a6957181b1458bc5d64eb6a7ab31cc19a314e13
Emmanuel Deloget <logout@free.fr>
2014-01-27 10:09:56 +0100

    config: document multi_v7_config

--- configs/multi_v7_config.sh
+++ configs/multi_v7_config.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+# This sample configuration is suitable to many ARMv7 boards. The used
+# kernel configuration is enough to boot the following processors:
+#  - Allwinner's A10, A13, A20
+#  - Some recent rockchip processors
+#  - Marvell's Armada 370, Armada XP
+#  - NVIDIA's Tegra 1, 2, 3
+#  - TI's Omap 3, Omap 4, Omap 5
+#  - Freescale's IMX53, IMX6Q, IMX6SL
+# And probably many more (more are added in each new kernel)
+#
+# The choosen configuration forces the use of the internal FPU and
+# is suitable to generate hard-float code using the latest ARM EABI
+# (ABI5). It generates code using the musl standard C library
+# (see http://musl-libc.org/ for further information).
+
 TARGET="armeb-linux-musleabihf"
 
 GMP_VERSION=5.1.3
